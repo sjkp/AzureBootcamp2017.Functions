86f8c6a96fb2c8a479478e2cc6cc86780c6e2320
Simon J.K. Pedersen <mail@sjkp.dk>
2017-04-21 23:48:07 +0200

    function demo.

---
+++ FunctionApp1/MyCoolClass.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApp
+{
+    public static class MyCoolClass
+    {
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello  from precompiled " + name);
+        }
+    }
+}
